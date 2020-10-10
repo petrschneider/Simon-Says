c630481a41174a5cfd38f158edeba0832578e080
Petr Schneider <Petr.S>
2020-10-10 08:49:01 +0200

    first commit

---
+++ game.js
@@ -0,0 +1,12 @@
+var gamePattern = [];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence(){
+var randomNumber = Math.floor(Math.random() * 4);
+return randomNumber;
+}
+
+var randomChosenColour = buttonColours[nextSequence()];
+gamePattern.push(randomChosenColour);
+
